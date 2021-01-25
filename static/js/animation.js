62af536c1f08bce057df9d8abbbd31d517ba4293
rstJkee <maximkostenko123@gmail.com>
2021-01-25 20:19:28 +0400

    Deleted old ugly menu-btn animation and added togleClass

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,8 +1,6 @@
 $(document).ready(() => {
     $("#btnMenu").on("click", () => {
-        $("#btnMenu > div:nth-child(1)").toggleClass("first-div-rotate")
-        $("#btnMenu > div:nth-child(2)").toggleClass("second-div-rotate")
-        $("#btnMenu > div:nth-child(3)").toggleClass("third-div-none")
+        $("#btnMenu").toggleClass("btn-outline-primary").toggleClass("btn-primary")
         $("#menu-body").toggleClass("open").toggleClass("menu-body-close")
     })
 })
