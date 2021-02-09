ba105b4d287e20a115c721b2d97abbb924627a69
rstJkee <maximkostenko123@gmail.com>
2021-02-09 09:54:18 +0400

    Added settings

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import *
 from modules.Database import *
 import webbrowser
+import os
 
 app = Flask(__name__)
 Passwords.create_table()
+settings_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "settings"+os.sep+"settings.json")
 
 
 @app.route("/")
@@ -11,20 +13,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/get_categories", methods=["GET"])
+@app.route("/categories", methods=["GET"])
 def get_categories():
     categories = search_categories()
     return json.dumps({"message": categories})
 
 
-@app.route("/get_passwords", methods=["GET"])
+@app.route("/password", methods=["GET"])
 def get_passwords():
     category = request.values.get("category")
     passwords = search_passwords_by_category(category)
     return json.dumps({"message": passwords})
 
 
-@app.route("/add_password", methods=["POST"])
+@app.route("/password", methods=["POST"])
 def add_password():
     message = add_password_to_db(request.values.get("category"), request.values.get("login"),
                                  request.values.get("password"), request.values.get("mail") or None,
@@ -32,7 +34,7 @@ def add_password():
     return json.dumps({"message": message})
 
 
-@app.route("/update_password", methods=["PUT"])
+@app.route("/password", methods=["PUT"])
 def update_password():
     message = update_password_bd(request.values.get("id"), request.values.get("login"),
                                  request.values.get("password"), request.values.get("mail"),
@@ -40,21 +42,27 @@ def update_password():
     return json.dumps({"message": message})
 
 
-@app.route("/delete_password", methods=["DELETE"])
+@app.route("/password", methods=["DELETE"])
 def delete_password():
     message = delete_password_bd(request.values.get("id"))
     return json.dumps({"message": message})
 
 
-@app.route("/get_settings", methods=["GET"])
+@app.route("/settings", methods=["GET"])
 def get_settings():
-    with open("settings/settings.json", "r") as st:
+    with open(settings_path, "r") as st:
         return json.dumps(json.load(st))
 
 
-@app.route("/set_settings", methods=["POST"])
+@app.route("/settings", methods=["PUT"])
 def set_settings():
-    settings = request.values
+    settings = request.values.get("autoCopy")
+    with open(settings_path, "r") as file:
+        data = json.load(file)
+        data["autoCopy"] = settings
+    with open(settings_path, "w") as file:
+        file.write(json.dumps(data))
+    return json.dumps({"message": "good"})
 
 
 def main():
