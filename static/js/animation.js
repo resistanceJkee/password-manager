ca90f68d408ed0a399efdc64ec5fb212359afa1c
rstJkee <maximkostenko123@gmail.com>
2021-02-23 20:09:21 +0400

    Added default category

--- static/js/animation.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-    $("#btnMenu").on("click", () => {
-        $("#btnMenu").toggleClass("btn-outline-primary").toggleClass("btn-primary")
-        $("#menu-body").toggleClass("open").toggleClass("menu-body-close")
-    })
-})
