24a105e300e6fa9fc5909d4b934f0364caa9160e
rst <maximkostenko123@gmail.com>
2021-03-29 14:56:31 +0400

    Added import/export to routing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from threading import Thread
 from modules.Database import *
 import webbrowser
 import os
+import modules.MoveDB
 
 app = Flask(__name__)
 Passwords.create_table()
@@ -68,6 +69,36 @@ def set_settings():
     with open(settings_path, "w") as file:
         file.write(json.dumps(data))
     return json.dumps({"message": "good"})
+
+
+@app.route("/import_key", methods=["GET"])
+def import_key():
+    return modules.MoveDB.import_some_key()
+
+
+@app.route("/export_key", methods=["GET"])
+def export_key():
+    return modules.MoveDB.export_some_key()
+
+
+@app.route("/import_db", methods=["GET"])
+def import_db():
+    return modules.MoveDB.import_some_db()
+
+
+@app.route("/export_db", methods=["GET"])
+def export_db():
+    return modules.MoveDB.export_some_db()
+
+
+@app.route("/import_all", methods=["GET"])
+def import_all():
+    return modules.MoveDB.import_all_files()
+
+
+@app.route("/export_all", methods=["GET"])
+def export_all():
+    return modules.MoveDB.export_all_files()
 
 
 def st_sv():
